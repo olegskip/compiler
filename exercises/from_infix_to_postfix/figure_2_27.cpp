4ce9ca3ba6b9581f381224a9a038308d35db2e73
Oleg Skip <skip.cpp.reg@gmail.com>
2022-03-04 10:52:48 +0200

    Add a simple translator from infix to postfix

---
+++ exercises/from_infix_to_postfix/figure_2_27.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <exception>
+#include <string>
+
+
+class Parser
+{
+public:
+	Parser(std::string str_): str(str_)
+	{
+		expr();
+		std::cout << "\n";
+	}
+
+private:
+	void expr()
+	{
+		term();
+		while(lookaheadIndex < str.size())
+		{
+			if(str[lookaheadIndex] == '+') {
+				match('+');
+				term();
+				std::cout << "+ ";
+			}
+			else if(str[lookaheadIndex] == '-') {
+				match('-');
+				term();
+				std::cout << "- ";
+			}
+			else {
+				error();
+			}
+		}
+	}
+
+	void term()
+	{
+		digit();
+		std::cout << str[lookaheadIndex] << " ";
+		match(str[lookaheadIndex]);
+	}
+
+	void digit()
+	{
+		if(lookaheadIndex >= str.size())
+			error();
+		if(!isdigit(str[lookaheadIndex]))
+			error();
+	}
+
+
+	void match(char ch)
+	{
+		if(str[lookaheadIndex] == ch) {
+			lookaheadIndex++;
+		}
+		else {
+			error();
+		}
+	}
+
+	void error()
+	{
+		throw std::logic_error("");
+	}
+
+	size_t lookaheadIndex = 0;
+	std::string str;
+};
+
+
+int main()
+{
+	const std::string str = "1+2-3";
+	Parser parser(str);
+
+	return 0;
+}
